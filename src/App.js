eec17aa4de1a43f5f2fbad542f8ab2558543bebd
Rohit Malhotra <rohitmalhotra@urbanclap.com>
2021-10-22 23:28:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import { Demo } from './graph'
-
+const data = {
+  "service_name": "growth_service",
+  "request": {},
+  "response": {},
+  "childs": [{
+    "service_name": "customer-pre-request-service",
+    "request": {},
+    "response": {},
+    "childs": [{
+      "service_name": "cart-service",
+      "request": {},
+      "response": {},
+      "childs": [{
+        "service_name": "catalog-service",
+        "request": {},
+        "response": {}
+      }]
+    },
+      {
+        "service_name": "catalog-service",
+        "request": {},
+        "response": {}
+      }
+    ]
+  }]
+}
 function App() {
   return (
     <div className="App">
-      <Demo></Demo>
+      <Demo data={data}></Demo>
     </div>
   );
 }
-
 export default App;
--- src/graph.js
+++ src/graph.js
@@ -1,71 +1,76 @@
 import { EuiTreeView, EuiToken, EuiIcon } from '@elastic/eui';
 import '@elastic/eui/dist/eui_theme_dark.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import _ from 'lodash'
 
-const Demo = () => {
-  const items = [
-    {
-      label: 'Item One',
-      id: 'item_one',
-      icon: <EuiIcon type="folderClosed" />,
-      iconWhenExpanded: <EuiIcon type="folderOpen" />,
-      isExpanded: true,
-      children: [
-        {
-          label: 'Item A',
-          id: 'item_a',
-          icon: <EuiIcon type="document" />,
-        },
-        {
-          label: 'Item B',
-          id: 'item_b',
-          icon: <EuiIcon type="arrowRight" />,
-          iconWhenExpanded: <EuiIcon type="arrowDown" />,
-          children: [
-            {
-              label: 'A Cloud',
-              id: 'item_cloud',
-              icon: <EuiToken iconType="tokenConstant" />,
-            },
-            {
-              label: "I'm a Bug",
-              id: 'item_bug',
-              icon: <EuiToken iconType="tokenEnum" />,
-              callback: () => {},
-            },
-          ],
-        },
-        {
-          label: 'Item C',
-          id: 'item_c',
-          icon: <EuiIcon type="arrowRight" />,
-          iconWhenExpanded: <EuiIcon type="arrowDown" />,
-          children: [
-            {
-              label: 'Another Cloud',
-              id: 'item_cloud2',
-              icon: <EuiToken iconType="tokenConstant" />,
-            },
-            {
-              label:
-                'This one is a really long string that we will check truncates correctly',
-              id: 'item_bug2',
-              icon: <EuiToken iconType="tokenEnum" />,
-              callback: () => {},
-            },
-          ],
-        },
-      ],
-    },
-    {
-      label: 'Item Two',
-      id: 'item_two',
-    },
-  ];
+function getChildData(data) {
+  return _.map(data, child => {
+    return getData(child)
+  })
+}
 
+function getData(data) {
+  if (!data) {
+    return {};
+  }
+
+  return {
+    label: data['service_name'],
+    id: data['service_name'] + data['method_name'],
+    icon: <EuiIcon type="folderClosed" />,
+    iconWhenExpanded: <EuiIcon type="folderOpen" />,
+    children: [getRequestChild(data), getResponseChild(data), getChildApis(data)]
+  }
+}
+
+function getChildApis(data) {
+  return {
+    label: "Apis",
+    id: data['service_name'] + data['method_name'] + "childApis",
+    icon: <EuiIcon type="folderClosed" />,
+    iconWhenExpanded: <EuiIcon type="folderOpen" />,
+    children: getChildData(data['childs'])
+  }
+}
+
+function getRequestChild(data) {
+  return {
+    label: "Request",
+    id: data['service_name'] + data['method_name'] + "request",
+    icon: <EuiIcon type="folderClosed" />,
+    iconWhenExpanded: <EuiIcon type="folderOpen" />,
+  }
+}
+
+function getResponseChild(data) {
+  return {
+    label: "Response",
+    id: data['service_name'] + data['method_name'] + "response",
+    icon: <EuiIcon type="folderClosed" />,
+    iconWhenExpanded: <EuiIcon type="folderOpen" />,
+  }
+}
+
+const Demo = (props) => {
+  // const data = props.data
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('http://localhost:8000/transactionLogs/getLogsByUserName?userName=rohit')
+      .then(response => response.json())
+      .then(data => {
+        console.log('API response:', data);
+        setData(data);
+      });
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+  
   return (
     <div style={{ width: '20rem' }}>
-      <EuiTreeView items={items} aria-label="Sample Folder Tree" />
+      <EuiTreeView items={[getData(data)]} aria-label="Sample Folder Tree" />
     </div>
   );
 };
